eb2faed3298b4ffa3973fa2873ad58dc3c8a945a
sustained <sustained.dissonance@gmail.com>
2018-11-26 21:13:16 +0000

    Add /languages "API" endpoint.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,8 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/languages', function () {
+    return [
+        ['id' => 0, 'name' => 'English', 'difficulty' => 4],
+        ['id' => 1, 'name' => 'Swedish', 'difficulty' => 3],
+        ['id' => 2, 'name' => 'German', 'difficulty' => 7]
+    ];
 });
