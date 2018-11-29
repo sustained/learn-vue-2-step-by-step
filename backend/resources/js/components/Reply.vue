221a199f7bf815713a02aca2bf338fac3e6ce00c
sustained <sustained.dissonance@gmail.com>
2018-11-29 12:48:26 +0000

    Vue Subclassing - Add reply component (ep. 32).

---
+++ backend/resources/js/components/Reply.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<p>{{ reply.content }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ["data"],
+
+		data() {
+			return {
+				reply: this.data
+			};
+		}
+	};
+</script>
