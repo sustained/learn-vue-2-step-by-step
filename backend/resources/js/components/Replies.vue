b65d8a48c52d57f93fc5d5bc7912918fc478c2f9
sustained <sustained.dissonance@gmail.com>
2018-11-29 12:48:18 +0000

    Vue Subclassing - Extend Collection with Replies (ep. 32).

---
+++ backend/resources/js/components/Replies.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<div v-for="(reply, index) in items" :key="'reply' + index">
+			<reply :data="reply" @deleted="remove(index)"></reply>
+		</div>
+
+		<!-- <new-reply :endpoint="endpoint" @created="add"></new-reply> -->
+	</div>
+</template>
+
+<script>
+	import Reply from "./Reply";
+	// import NewReply from "./NewReply";
+	import Collection from "./Collection";
+
+	export default Collection.extend({
+		components: { Reply },
+
+		data() {
+			return {
+				endpoint: location.pathname + "/replies"
+			};
+		}
+	});
+</script>
